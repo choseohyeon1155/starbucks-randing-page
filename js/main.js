78774293c7d64a4fc2cd1c7450c3f96393d58062
seohyeon <whtjgus112@naver.com>
2024-12-23 00:14:32 +0900

    배너, 공지사항,  프로모션 작업업

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
+//search
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
 searchEl.addEventListener('click', function (){
     searchInputEl.focus();
-  });
+});
 
 searchInputEl.addEventListener('focus', function(){
     searchEl.classList.add('focused');
@@ -16,6 +17,7 @@ searchInputEl.addEventListener('blur', function(){
 });
 
 
+//badge
 const badgeEl = document.querySelector('header .badges');
 
 window.addEventListener('scroll', _.throttle(function(){
@@ -25,23 +27,74 @@ window.addEventListener('scroll', _.throttle(function(){
         //gsap.to(요소, 지속시간, 옵션)
         //badgeEl.style.display = 'none';
         gsap.to(badgeEl, .6, {
-            opacity: 0
+            opacity: 0,
+            display: 'none'
         });
     }else{
         //배지 보이기
         //badgeEl.style.display = 'block';
         gsap.to(badgeEl, .6, {
-            opacity: 1
+            opacity: 1,
+            display: 'block'
         });
     }
 }, 300));
 //_.throttle(함수, 시간)
+//시간 300 -> 0.3초
+//스크롤 시 핸들러(함수)를 실행하고, 0.3초 단위로 부화를 줌
 
 
+//visual fade
 const fadeEls = document.querySelectorAll('.visual .fade-in');
+
 fadeEls.forEach(function(fadeEl, index) {
     gsap.to(fadeEl, 1, {
-        delay: (index)+1 *.7,
+        delay: (index + 1) * .7,
         opacity: 1
     });
+});
+
+
+//Swiper
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',
+    autoplay: true,
+    loop: true
+});
+//new Swiper(요소, 옵션);
+
+//promotion
+new Swiper('.promotion .swiper-container', {
+    //direction: 'horizontal',  -> 기본값
+    slidesPerView: 3,    //한번에 보여줄 슬라이드 개수
+    spaceBetween: 10,    //슬라이드 사이 여백
+    centeredSlides: true,    //1번 슬라이드가 가운데 보이기
+    loop: true,
+    autoplay: {
+        delay: 5000    //5초
+    },
+    pagination: {
+        el: '.promotion .swiper-pagination',    //페이지 번호 요소 선택자
+        clickable: true    //사용자의 페이지 번호 요소 제어 기능 여부
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next'
+    }
+});
+
+//promotion ToggleBtn
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function (){
+    isHidePromotion = !isHidePromotion
+    if(isHidePromotion){
+        //숨김 처리
+        promotionEl.classList.add('hide');
+    }else{
+        //보임 처리
+        promotionEl.classList.remove('hide');
+    }
 });
