fb65f37b5776b20cbe5b52040d7ba908d73a275d
seohyeon <whtjgus112@naver.com>
2025-01-01 17:18:09 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-//search
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function (){
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function(){
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function(){
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
-
-
 //badge, to-top
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -170,7 +151,3 @@ spyEls.forEach(function (spyEl) {
         .setClassToggle(spyEl, 'show')
         .addTo(new ScrollMagic.Controller());
 });
-
-//this-year
-const thisyear = document.querySelector('.this-year');
-thisyear.textContent = new Date().getFullYear();
